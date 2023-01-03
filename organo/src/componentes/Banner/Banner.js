24109dd7040f3758effbeed0bedb1c0ca4051183
Brunno Terada <brunoterada99@gmail.com>
2023-01-03 20:10:19 -0300

    Banner
    Adicionando um banner no projeto

---
+++ organo/src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css';
+
+function Banner() {
+    return(
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner princiapl da página do Organo"/>
+        </header>
+    )
+}
+
+document.createElement('img')
+
+export default Banner
