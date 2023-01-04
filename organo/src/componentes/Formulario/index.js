d9d08fd9011e7514e3657388cf4c2937c2d78dd1
Brunno Terada <brunoterada99@gmail.com>
2023-01-04 20:42:59 -0300

    Atualizando formulário
    Atualização no estilo do formulario

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -2,8 +2,9 @@ import CampoTexto from '../CampoTexto/CampoTexto'
 import './Formulario.css'
 const Formulario = () =>{
     return(
-        <section>
+        <section className='formulario'>
             <form>
+                <h2> Preencha os dados para criar um card dos amigos</h2>
                 <CampoTexto label='Nome' placeholder='Digite seu Nome'/>
                 <CampoTexto label='Cargo' placeholder='Digite seu cargo'/>
                 <CampoTexto label='Imagem' placeholder='Digite o caminho da imagem'/>
