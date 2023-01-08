5a5452ba2012344e7a9fc0ceea0aff63b9abb55a
Brunno Terada <brunoterada99@gmail.com>
2023-01-08 20:54:21 -0300

    Salvando os colaboradores em lista

--- organo/src/App.js
+++ organo/src/App.js
@@ -5,7 +5,7 @@ import Formulario from './componentes/Formulario';
 
 function App() {
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
   }
   const [colaboradores, setColaboradore] = useState([])
 
